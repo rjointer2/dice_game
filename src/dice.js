f4e8c7a6f249fa25f365af84a52729eb12c76942
Roody <rjointer2@gmail.com>
2021-03-21 10:23:51 -0700

    npm intsalled react-app set up

---
+++ src/dice.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dice extends Component {
+    render() {
+        return (
+            <div>
+                <p>Dice</p>
+            </div>
+        )
+    }
+}
+
+export default Dice
